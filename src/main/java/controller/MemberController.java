230a1ee473520ff1681d0dbba7aaaa790301110f
yoonsic2 <mrk1251@naver.com>
2024-12-04 16:20:49 +0900

    16:20

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -7,13 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.MemberService;
 
-@WebServlet({"/main", "/joinfrm", "/join", "/login", "/logout"})  // "/" 로 main은 못 감 _ 기본적으로 서블릿(default servlet(정적))이 쓰고있음
+
+@WebServlet({"/main", "/joinfrm", "/join", "/loginfrm", "/login", "/logout"})  // "/" 로 main은 못 감 _ 기본적으로 서블릿(default servlet(정적))이 쓰고있음
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	req.setCharacterEncoding("utf-8");
     	String cmd = req.getServletPath();
     	System.out.println("cmd="+cmd);
     	
@@ -32,6 +35,14 @@ public class MemberController extends HttpServlet {
     	case "/join":
     		//DB에 회원가입 하기
     		path = mSer.join(); //회원가입 성공: loginfrm.jsp , 실패: joinfrm.jsp
+    		break;
+    	
+    	case "/loginfrm":
+    		path="loginfrm.jsp";
+    		break;
+    		
+    	case "/login":
+    		path=mSer.login();
     		break;
     	}
     	
