72faf946b665aa7485d8b1e76b191bd984ce82e5
eun <lovepek11@naver.com>
2024-12-04 16:11:50 +0900

    oracle
    ojdbc~

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -10,39 +10,37 @@ import javax.servlet.http.HttpServletResponse;
 import service.MemberService;
 
 // "/" default servlet 정적리소스 사용하기 위함.
-@WebServlet({"/main","/joinfrm","/join","/loginfrm","/login","/logout"})
+@WebServlet({ "/main", "/joinfrm", "/join", "/loginfrm", "/login", "/logout" })
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  @Override
-protected void service(HttpServletRequest req, 
-		HttpServletResponse resp) 
-				throws ServletException, IOException {
-	  String cmd = req.getServletPath();
-	  System.out.println("cmd="+cmd);
-	  String path=null;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		String cmd = req.getServletPath();
+		System.out.println("cmd=" + cmd);
+		String path = null;
 
 		MemberService mSer = new MemberService(req, resp);
-	  switch (cmd) {
-		case "/joinfrm": 
-			//회원가입 창열기전에 인증확인
-			path="joinfrm.jsp";
+		switch (cmd) {
+		case "/joinfrm":
+			// 회원가입 창열기전에 인증확인
+			path = "joinfrm.jsp";
 			break;
 		case "/join":
-			//db에 회원가입하기
-			mSer.join();  //회원가입 성공 : loginfrm.jsp, 실패 : joinfrm.jsp
+			// db에 회원가입하기
+			mSer.join(); // 회원가입 성공 : loginfrm.jsp, 실패 : joinfrm.jsp
 			break;
 		case "/loginfrm":
-			path="loginfrm.jsp";
+			path = "loginfrm.jsp";
 			break;
 		case "/login":
 			path = mSer.login();
 			break;
-	
 
+		}
+
+		// 포워딩
+		req.getRequestDispatcher(path).forward(req, resp);
 	}
-	  
-	  //포워딩
-	  req.getRequestDispatcher(path).forward(req, resp);
-  }
 }
--- src/main/java/dao/MemberDao.java
+++ src/main/java/dao/MemberDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import dto.Member;
 
@@ -11,16 +12,36 @@ public class MemberDao {
 	Connection con;
 	PreparedStatement stmt;
 	ResultSet rs;
+
 	public void connect() {
-		con=JdbcUtil.getConnection();
-		
+		con = JdbcUtil.getConnection();
+
 	}
+
 	public void close() {
 		JdbcUtil.close(rs, stmt, con);
-		
+
 	}
+
 	public boolean join(Member member) {
-		// TODO Auto-generated method stub
+		String sql = "insert into member(username, userpw, irum, gender)" + "values (?,?,?,?)";
+		try {
+			stmt = con.prepareStatement(sql); // 파싱
+			stmt.setString(1, member.getUsername());
+			stmt.setString(2, member.getUserpw());
+			stmt.setString(3, member.getIrum());
+			stmt.setString(4, member.getGender());
+			int result = stmt.executeUpdate();// insert, update, delete
+			if (result > 0) {
+				System.out.println("join Dao 성공");
+				return true;
+			} else {
+				System.out.println("join dao 실패");
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 }
